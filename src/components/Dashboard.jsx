0280f2b64724f7341b669d67e73a95031dd5358e
XinGao <116980429+XinGao0424@users.noreply.github.com>
2023-12-04 13:29:54 -0500

    Upload generate currency in the surfer page

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,19 @@
-import { StyledTitle, StyledSubTitle, Avatar, StyledButton, ButtonGroup,StyledFormArea,colors} from "../components/Styles";
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { StyledTitle, StyledButton, StyledFormArea, Avatar, ButtonGroup } from '../components/Styles';
 import Logo from "../Assets/logo.png";
-import { getAuth,signOut } from "firebase/auth";
-const Dashboard = ()=> {
-    const history = useNavigate();
+import { useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from "firebase/auth";
 
+const Dashboard = () => {
+    const history = useNavigate();
+    const auth = getAuth();
+    const [balance, setBalance] = useState(0);
+
+    const generateCurrency = () => {
+        setBalance(prevBalance => prevBalance + 1);
+    };
 
     const logout = async () => {
-        const auth = getAuth();
-    
         try {
             // Perform any additional logout logic specific to your application
     
@@ -23,29 +27,31 @@ const Dashboard = ()=> {
             // Handle error as needed
         }
     };
-    
 
-    return(
-     <div>
-        <div 
-         style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            backgroundColor: "transparent",
-            width: "100%",
-            padding: "15px",
-            display: "flex",
-            justifyContent: "flex-start",
-
-           }}>
-            <Avatar image={Logo}/>
-            </div> 
-            <StyledFormArea bg ="transparent">
-                <StyledTitle size={65}> Welcome To SnapTweet</StyledTitle>
+    return (
+        <div>
+            <div
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    backgroundColor: 'transparent',
+                    width: '100%',
+                    padding: '15px',
+                    display: 'flex',
+                    justifyContent: 'flex-start',
+                }}
+            >
+                <Avatar image={Logo} />
+            </div>
+            <StyledFormArea bg="transparent">
+                <StyledTitle size={65}>Welcome To SnapTweet</StyledTitle>
+                <div style={{ textAlign: 'center', marginTop: '30px' }}>
+                    <StyledButton onClick={generateCurrency}>Generate Currency</StyledButton>
+                    <p>Balance: ${balance.toFixed(2)}</p>
+                </div>
                 <ButtonGroup><StyledButton onClick={logout} to="#">Logout</StyledButton></ButtonGroup>
             </StyledFormArea>
-
         </div>
     );
 };
