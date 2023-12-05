479f21cc337eb32668e5e4427bf4012a1f65eaba
Zhanbin Wang <112278341+CW1217@users.noreply.github.com>
2023-12-05 13:45:13 -0500

    Update Search.jsx
    bug fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import "./Search.css"
+import "./Search.css";
 import { SearchBar } from './SearchBar';
-import { SearchResultList } from './SearchResultsList';
-
+import { SearchResultsList } from './SearchResultsList';
+import UserData from "./UserData.json"
+import NavBar from './NavBar';
+import { Link } from 'react-router-dom';
 //The search page, you get to here by cliking the button on the nav bar
 //includes a search bar and the list of results
 export function Search () {
     const [results, setResults] = useState([]);
     return (
         <div className="Search">
-            <div className="searchBarContainer">
-                <SearchBar setResults={setResults}/>
-                <SearchResultList results={results} />
-            </div>
+          <div className="searchBarContainer">
+            <SearchBar setResults={setResults} data={UserData}/>
+            {results && results.length > 0 && <SearchResultsList results={results} />}
+          </div>
         </div>
-    )
+    );
 }
