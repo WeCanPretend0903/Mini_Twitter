cbb6067fd98b80c2c7a219e1aed86d89ea7534d8
Zhanbin Wang <112278341+CW1217@users.noreply.github.com>
2023-12-05 13:49:13 -0500

    Update SearchResult.jsx
    update link to home page

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import "./SearchResultsList.css"
+import "./SearchResult.css";
+import { Link } from 'react-router-dom';
 
 //1.for the result list to look better under the search bar, see the css file
 //2.for the user to  click on the result list items
 export const SearchResult = ({ result }) => {
+    const linkStyle = {
+        textDecoration: 'none',
+        color: 'inherit',
+        cursor: 'pointer',
+    };
     return (
         //change this to the link to post or user
-        <div className="searchResult" onClick={(e) => alert(`You clicked on ${result.name}`)}>
-            {result.name}
+        <Link to="/home" style={linkStyle}>
+        <div className="searchResult">
+          {result}
         </div>
+      </Link>
     );
 };
