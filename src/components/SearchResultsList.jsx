895376ab7a2159f567ad014a8e64cfd6f416ebce
Zhanbin Wang <112278341+CW1217@users.noreply.github.com>
2023-12-05 13:50:18 -0500

    Update SearchResultsList.jsx

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./SearchResultsList.css"
+import { SearchResult } from "./SearchResult";
 
 //this is the list of the search results, 
-export const SearchResultList = ({ results }) => {
+export const SearchResultsList = ({ results }) => {
     return (
         <div className="resultsList">
-            {
-                results.map((result, id) => {
-                    return <div key={id}>{result.name}</div>;
-                })
-            }
+          {results.map((result, id) => {
+            return <SearchResult result={result.name} key={id} />;
+          })}
         </div>
     );
 };
