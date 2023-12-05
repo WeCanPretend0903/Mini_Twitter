0670a6b045698f1fe9ac76534c35b75eb880c631
Zhanbin Wang <112278341+CW1217@users.noreply.github.com>
2023-12-05 13:48:14 -0500

    Update SearchBar.jsx
    fix the result list not showing;
     now data are from json

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import {FaSearch} from "react-icons/fa"
-import "./SearchBar.css"
+import { FaSearch } from "react-icons/fa";
+import "./SearchBar.css";
 
-export const SearchBar = ({ setResults }) => {
-    const [input, setInput] = useState("") 
+export const SearchBar = ({ setResults, data }) => {
+    const [input, setInput] = useState("");
     const fetchData = (value) => {
-        //we can put some kind of database for search, like username, posts, in json file
-        //I put some place holder for now
-        const data = ['user', 'user', 'post', 'post', 'content'];//fake data
-        //const results = data.filter(item => item.toLowerCase().includes(value.toLowerCase()));
-        const results = data;
-        setResults(results);
-    }
+      const results = data.user.filter((user) => {
+        return (
+          value &&
+          user &&
+          user.name &&
+          user.email &&
+          user.name.toLowerCase().includes(value)
+        );
+      });
+      setResults(results);
+    };
+    
     const handleInputChange = (value) => {
         setInput(value);
         fetchData(value);
-    }
+    };
     return (
         <div className="inputWrapper">
             <FaSearch id="searchIcon" />
-            <input id="search-input"placeholder="Search here..." 
+            <input id="searchInput"placeholder="Search here..." 
             value={input}
             onChange={(e) => handleInputChange(e.target.value)} //store the typing inside input variable (存input)
             />
