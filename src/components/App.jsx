2d2d632ee2bf8ed8e717e1a7a64ba51723c0aebb
Reagan Dhar <116980504+ReaganD1@users.noreply.github.com>
2023-12-07 14:41:04 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,10 @@ import Login from "./Login";
 import Signup from "./SignUp";
 import Dashboard from "./Dashboard";
 import Weather from "./Weather";
+import Profile from "./profile";
 import { StyledContainer } from "./Styles";
 import { Search } from "./Search";
+import Warning from "./Warning_Wrapper";
 
 function App() {
   // Define the signupUser function
@@ -20,26 +22,134 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<StyledContainer><Signup signupUser={signupUser} /></StyledContainer>} />
-        <Route path="/login" element={<StyledContainer><Login /></StyledContainer>} />
-        <Route path="/dashboard" element={
-        <div>
-          <NavBar/>
-          <StyledContainer><Dashboard /></StyledContainer>
-        </div>} />
-        <Route path="/" element={<StyledContainer><MainHome /></StyledContainer>} />
-        <Route path="/search" element={<StyledContainer><Search /></StyledContainer>} />
-        <Route path="/weather" element={
-         <div>
-         <NavBar/>
-        <StyledContainer><Weather /></StyledContainer>
-        </div>} />
-        <Route path="/home" element={
-          <div>
-            <NavBar />
-            <HomePage />
-          </div>
-        }
+        <Route
+          path="/login"
+          element={
+            <StyledContainer>
+              <Login />
+            </StyledContainer>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <StyledContainer>
+              <Signup signupUser={signupUser}/>
+            </StyledContainer>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <div>
+              <NavBar />
+              <StyledContainer>
+                <Dashboard />
+              </StyledContainer>
+            </div>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <StyledContainer>
+              <MainHome />
+            </StyledContainer>
+          }
+        />
+        <Route
+          path="/warning"
+          element={
+            <div>
+              <NavBar />
+              <StyledContainer>
+                <Warning />
+              </StyledContainer>
+            </div>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <StyledContainer>
+              <Search />
+            </StyledContainer>
+          }
+        />
+        <Route
+          path="/weather"
+          element={
+            <div>
+              <NavBar />
+              <StyledContainer>
+                <Weather />
+              </StyledContainer>
+            </div>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <div>
+              <NavBar />
+              <HomePage />
+            </div>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <StyledContainer>
+              <Login />
+            </StyledContainer>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <div>
+              <NavBar />
+              <StyledContainer>
+                <Dashboard />
+              </StyledContainer>
+            </div>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <StyledContainer>
+              <MainHome />
+            </StyledContainer>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <StyledContainer>
+              <Search />
+            </StyledContainer>
+          }
+        />
+        <Route
+          path="/weather"
+          element={
+            <div>
+              <NavBar />
+              <StyledContainer>
+                <Weather />
+              </StyledContainer>
+            </div>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <div>
+              <NavBar />
+              <HomePage />
+            </div>
+          }
         />
       </Routes>
     </Router>
@@ -47,3 +157,4 @@ function App() {
 }
 
 export default App;
+
