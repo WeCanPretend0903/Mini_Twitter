4ee07d2b02af074aadb1c6bbec3cb435100f0e39
Zhanbin Wang <112278341+CW1217@users.noreply.github.com>
2023-12-07 14:54:11 -0500

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
+import { useNavigate } from 'react-router-dom';
 
-export const SearchBar = ({ setResults, data }) => {
+export const SearchBar = ({setResults, data }) => {
     const [input, setInput] = useState("");
+    //const [results, setResults1] = useState([]);
+    //const navigate = useNavigate();
+
     const fetchData = (value) => {
       const results = data.users.filter((users) => {
         return (
@@ -20,17 +24,24 @@ export const SearchBar = ({ setResults, data }) => {
         setInput(value);
         fetchData(value);
     };
+/*
+    const navigateToResultsPage = () => {
+      // You can use the useHistory hook to navigate to another page
+      navigate('/result', { state: { results } });
+    };
+*/
     return (
         <div className="inputWrapper">
             <FaSearch id="searchIcon" />
-            <input id="searchInput"placeholder="Search here..." 
+            <input id="searchInput"placeholder="Search or Type an User name" 
             value={input}
-            onChange={(e) => handleInputChange(e.target.value)} //store the typing inside input variable (存input)
+            onChange={(e) => handleInputChange(e.target.value)} //store the typing inside input variable
             />
+            {/*<button onClick={navigateToResultsPage}>Go to Results</button>*/}
         </div>
     );
 }
 /*Reference:
-The search function is made based on the tutorial video:
+The search function is made based on tutorial video:
 "Make a Search Bar with React (with API Calls) | Beginners Tutorial" by Code Complete
 from Youtube, link: https://www.youtube.com/watch?v=sWVgMcz8Q44 */
